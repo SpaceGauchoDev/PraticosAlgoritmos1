f66390236f8f42cf23a9a838544f9e9d26a179d2
manuel.dearmas <manuel.dearmas@globant.com>
2019-09-29 11:58:32 -0300

    setting up projects structure

---
+++ Practico_2/src/mda/practico_2/Main.java
@@ -0,0 +1,8 @@
+package mda.practico_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Practico_5/src/mda/practico_5/Main.java
@@ -0,0 +1,14 @@
+package mda.practico_5;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+    }
+
+
+
+}
